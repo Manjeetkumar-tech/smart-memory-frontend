d3538e8fc898660648ee8fd9994f2f3266895573
Manjeetkumar-tech <manjeetkumarjnv968@gmail.com>
2025-12-13 22:11:24 +0900

    fix: leaflet map image missing issue

--- src/components/MapDisplay.vue
+++ src/components/MapDisplay.vue
@@ -6,6 +6,16 @@
 import { onMounted, onUnmounted, ref, watch } from 'vue'
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+
+delete L.Icon.Default.prototype._getIconUrl
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  iconRetinaUrl: markerIcon2x,
+  shadowUrl: markerShadow,
+})
 
 const props = defineProps({
   latitude: {
